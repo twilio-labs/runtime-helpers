fd2a242c44ea95e1bfa7b8835dc7442230109605
Jordan Mulcahey <jmulcahey@twilio.com>
2021-09-20 05:09:36 -0700

    feat: event-related functions

---
+++ src/event.ts
@@ -0,0 +1,53 @@
+/**
+ * Functions that operate on Serverless `event` objects.
+ * @module
+ */
+import '@twilio-labs/serverless-runtime-types';
+
+type Parameters = {
+  [key: string]: string;
+};
+
+/**
+ * Extracts the HTTP parameters from a Serverless `event`. Can optionally accept a paramter
+ * `required`, which is a list of required parameter names that the request to the Function should have
+ * included. If any required parameters are missing, this function throws an error`Response` that should
+ * be caught and passed to a Serverless Function's `callback`. If `moreInfo` is defined, it should be a
+ * documentation URL describing the Function's API.
+ *
+ * @param event A Serverless event.
+ * @param required A list of required parameter keys.
+ * @param moreInfo An optional URL to the Function's API docs, to be included in error responses.
+ */
+
+export function extractParams(
+  event: Parameters,
+  required: string[] = [],
+  moreInfo: string | undefined = undefined
+) {
+  const missingParams = required.reduce((acc: string[], param: string) => {
+    if (typeof event[param] === 'undefined') {
+      acc.push(param);
+    }
+    return acc;
+  }, []);
+
+  if (missingParams.length > 0) {
+    const response = new Twilio.Response();
+
+    response.appendHeader('Content-Type', 'application/json');
+    response.setStatusCode(400);
+    response.setBody({
+      error: {
+        message: `Missing parameter; please provide: '${missingParams.join(
+          ', '
+        )}'.`,
+        moreInfo,
+      },
+    });
+
+    throw response;
+  } else {
+    return event;
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import * as environmentLib from './environment';
+import * as eventLib from './event';
 import * as utilsLib from './utils';
 
 export const environment = environmentLib;
+
+export const event = eventLib;
 
 export { requireAsset, requireFunction } from './utils';
 export const utils = utilsLib;

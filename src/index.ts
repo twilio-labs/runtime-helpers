409beb343e375249f91c51997d1605d0c823d4f2
Jordan Mulcahey <jmulcahey@twilio.com>
2021-09-22 17:53:13 -0700

    feat(response): enableCORS function

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import * as environmentLib from './environment';
 import * as eventLib from './event';
+import * as responseLib from './response';
 import * as utilsLib from './utils';
 
 export const environment = environmentLib;
-
 export const event = eventLib;
+export const response = responseLib;
 
 export { requireAsset, requireFunction, success, failure } from './utils';
 export const utils = utilsLib;
---
+++ src/response.ts
@@ -0,0 +1,26 @@
+/**
+ * Functions that operate on Serverless `TwilioResponse` objects.
+ * @module
+ */
+import { TwilioResponse } from '@twilio-labs/serverless-runtime-types/types';
+
+/**
+ * Modifies a `TwilioResponse` to include CORS headers.
+ *
+ * The `origin` parameter is required, but passing in `*` will allow access from any origin.
+ *
+ * @param origin A URL string representing an origin, or `*` for any origin.
+ * @param methods A list of HTTP methods whose use is allowed by clients.
+ * @param headers A list of HTTP headers that can be used in a client's request.*/
+export function enableCORS(
+  response: TwilioResponse,
+  origin: string,
+  methods: string[] = ['POST', 'OPTIONS'],
+  headers: string[] = ['Content-Type']
+) {
+  response.appendHeader('Access-Control-Allow-Origin', origin);
+  response.appendHeader('Access-Control-Allow-Methods', methods.join(', '));
+  response.appendHeader('Access-Control-Allow-Headers', headers.join(', '));
+
+  return response;
+}

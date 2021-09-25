d81e8b3cc961b6c1260386ce740c524190229011
Jordan Mulcahey <jmulcahey@twilio.com>
2021-09-24 17:49:33 -0700

    feat(auth): isAuthenticated function

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
+import * as authLib from './auth';
 import * as environmentLib from './environment';
 import * as eventLib from './event';
 import * as responseLib from './response';
 import * as utilsLib from './utils';
 
+export const auth = authLib;
 export const environment = environmentLib;
 export const event = eventLib;
 export const response = responseLib;

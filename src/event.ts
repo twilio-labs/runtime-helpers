71e8d1abeee11ab84df08e6c236ceef54035b2a1
Jordan Mulcahey <jmulcahey@twilio.com>
2021-10-29 18:05:21 -0700

    feat(event): Refactor parameter validation with ow

--- src/event.ts
+++ src/event.ts
@@ -3,53 +3,46 @@
  * @module
  */
 import '@twilio-labs/serverless-runtime-types';
+import { ServerlessCallback } from '@twilio-labs/serverless-runtime-types/types';
+import ow from 'ow';
 
 type Parameters = {
   [key: string]: string;
 };
 
 /**
- * Extracts the HTTP parameters from a Serverless `event`. Can optionally accept a paramter
- * `required`, which is a list of required parameter names that the request to the Function should have
- * included. If any required parameters are missing, this function throws an error `Response` that should
- * be caught and passed to a Serverless Function's `callback`. If `moreInfo` is defined, it should be a
- * documentation URL describing the Function's API.
+ * Ensures that a Serverless `event` has HTTP parameters defined for the keys
+ * given in the argument `required`. If any required parameters are missing, an
+ * error response is generated and passed to the given Serverless `callback`.
  *
- * Usage: `const {param1, param2, param3} = extractParams(event, ['param1', 'param2', 'param3']);`
+ * Usage: `expectParams(event, callback, ['param1', 'param2', 'param3']);`
  *
  * @param event A Serverless event.
+ * @param callback A Serverless callback.
  * @param required A list of required parameter keys.
  * @param moreInfo An optional URL to the Function's API docs, to be included in error responses.
  */
 
-export function extractParams(
-  event: Parameters,
-  required: string[] = [],
+export function expectParams(
+  event: unknown,
+  callback: ServerlessCallback,
+  required: string[],
   moreInfo: string | undefined = undefined
 ) {
-  const missingParams = required.reduce((acc: string[], param: string) => {
-    if (typeof event[param] === 'undefined') {
-      acc.push(param);
-    }
-    return acc;
-  }, []);
-
-  if (missingParams.length > 0) {
+  try {
+    ow(event, ow.object.hasKeys.apply(required).valuesOfType(ow.string));
+  } catch (error: any) {
     const response = new Twilio.Response();
 
     response.appendHeader('Content-Type', 'application/json');
     response.setStatusCode(400);
     response.setBody({
       error: {
-        message: `Missing parameter; please provide: '${missingParams.join(
-          ', '
-        )}'.`,
+        message: error.message,
         moreInfo,
       },
     });
 
-    throw response;
-  } else {
-    return event;
+    return callback(null, response);
   }
 }

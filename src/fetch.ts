c8a5cee12a45c2b80dc59a4e55f032ea50f8934a
Jordan Mulcahey <jmulcahey@twilio.com>
2021-11-16 17:31:43 -0800

    feat(fetch): Include fetch-retry types in docs

--- src/fetch.ts
+++ src/fetch.ts
@@ -1,5 +1,35 @@
 import rawFetch from 'cross-fetch';
-const fetchBuilder = require('fetch-retry').default;
+
+/**
+ * The type signature for {@link fetch}'s `retryDelay` function.
+ */
+
+export type RequestDelayFunction = (
+  attempt: number,
+  error: Error | null,
+  response: Response | null
+) => number;
+
+/**
+ * The type signature for {@link fetch}'s `retryOn` function.
+ */
+
+export type RequestRetryOnFunction = (
+  attempt: number,
+  error: Error | null,
+  response: Response | null
+) => boolean | Promise<boolean>;
+
+/**
+ * The set of options that can be passed to {@link fetch}, augmented with
+ * a number of non-standard fields related to retrying requests.
+ */
+
+export interface RequestInitWithRetry extends RequestInit {
+  retries?: number;
+  retryDelay?: number | RequestDelayFunction;
+  retryOn?: number[] | RequestRetryOnFunction;
+}
 
 /**
  * The default exponential backoff delay function used by
@@ -9,9 +39,18 @@ const fetchBuilder = require('fetch-retry').default;
 export const exponentialDelay = (attempt: number) =>
   Math.pow(2, attempt) * 1000;
 
+const fetchBuilder = (): ((
+  input: RequestInfo,
+  init?: RequestInitWithRetry
+) => Promise<Response>) =>
+  require('fetch-retry')(rawFetch, {
+    retries: 0,
+    retryDelay: exponentialDelay,
+  });
+
 /**
  * A cross-platform (Node and browser) implementation of the
- * (Fetch API)[https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API]. This
+ * [Fetch API](https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API). This
  * Fetch implementation supports automatic retries on failure with (by default)
  * exponential backoff, in addition to the standard Fetch API features.
  *
@@ -19,12 +58,9 @@ export const exponentialDelay = (attempt: number) =>
  *
  * The `options` object supports the following non-standard fields:
  *
- * `retries`: The number of retries to attempt (default 0; no retries)
- * `retryDelay`: A function or number used to calculate the number of milliseconds to delay between retries (default {@link exponentialDelay})
+ * `retries`: The number of retries to attempt (default 0; no retries)\
+ * `retryDelay`: A function or number used to calculate the number of milliseconds to delay between retries (default {@link exponentialDelay})\
  * `retryOn`: An array of HTTP status codes that cause a retry (defaults to the set of network-related status codes)
  */
 
-export const fetch = fetchBuilder(rawFetch, {
-  retries: 0, // Default to no retry
-  retryDelay: exponentialDelay,
-});
+export const fetch = fetchBuilder();

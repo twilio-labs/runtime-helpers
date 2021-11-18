6b0e608f3508ac435f32d68fd8438b65cc901c5c
Jordan Mulcahey <jmulcahey@twilio.com>
2021-11-17 20:32:07 -0800

    fix(response): Add Response object to test mock

--- src/__tests__/response.test.ts
+++ src/__tests__/response.test.ts
@@ -6,35 +6,35 @@ import {
 } from '@twilio-labs/serverless-runtime-types/types';
 import { createCORSResponse } from '../response';
 
+class Response implements TwilioResponse {
+  headers: { [key: string]: string };
+
+  constructor() {
+    this.headers = {};
+  }
+
+  appendHeader(key: string, value: string): void {
+    this.headers[key] = value;
+  }
+
+  setHeaders(headers: { [key: string]: string }): void {
+    this.headers = headers;
+  }
+
+  setStatusCode() {}
+  setBody() {}
+}
+
 describe('createCORSResponse()', () => {
   it('should create a new TwilioResponse with CORS headers', () => {
     const origin = 'example.org';
     const methods = ['GET', 'POST', 'OPTIONS'];
     const headers = ['X-Test'];
 
-    const responseMock = new Mock<TwilioResponse>()
-      .setup((instance) =>
-        instance.appendHeader('Access-Control-Allow-Origin', origin)
-      )
-      .returns(undefined)
-      .setup((instance) =>
-        instance.appendHeader(
-          'Access-Control-Allow-Methods',
-          methods.join(', ')
-        )
-      )
-      .returns(undefined)
-      .setup((instance) =>
-        instance.appendHeader(
-          'Access-Control-Allow-Headers',
-          headers.join(', ')
-        )
-      )
-      .returns(undefined)
-      .object();
+    const response = new Response();
     const responseConstructorMock = new Mock<ResponseConstructor>()
       .setup((instance) => new instance())
-      .returns(responseMock)
+      .returns(response)
       .object();
     globalThis.Twilio = new Mock<GlobalTwilio>()
       .setup((instance) => instance.Response)
@@ -42,5 +42,13 @@ describe('createCORSResponse()', () => {
       .object();
 
     createCORSResponse(origin, methods, headers);
+
+    expect(response.headers['Access-Control-Allow-Origin']).toBe(origin);
+    expect(response.headers['Access-Control-Allow-Methods']).toEqual(
+      methods.join(', ')
+    );
+    expect(response.headers['Access-Control-Allow-Headers']).toEqual(
+      headers.join(', ')
+    );
   });
 });
